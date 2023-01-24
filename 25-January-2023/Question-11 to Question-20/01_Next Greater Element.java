d5eecb1d8c2dab9d7d760e6b35aed0771421e611
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-01-24 17:33:28 +0530

    Update 01_Next Greater Element.java

--- 25-January-2023/Question-11 to Question-20/01_Next Greater Element.java
+++ 25-January-2023/Question-11 to Question-20/01_Next Greater Element.java
@@ -31,3 +31,34 @@ class Solution {
         return res;
     }
 }
+
+
+
+
+
+// less run time
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer>stack=new Stack<Integer>();
+        Map<Integer,Integer>map=new HashMap<Integer,Integer>();                    //            i      
+        int[]ans=new int[nums1.length];                                               // nums={1,3,4,2}
+        
+        for(int i:nums2){                                                            // |         |       if(st.peek()<i)  1<3
+            while(!stack.isEmpty()){                                                 // |__1______|        map.put(1,3)
+                if(stack.peek()<i){                                                  //    st^                st.pop()
+                    
+                    map.put(stack.peek(),i);                                          //    st.push(i)=push 3 in stack
+                    stack.pop();                                                   //   |         |       
+                }else break;                                                       //   |____3____|         st.peek()<i    3<4
+            }                                                                       //                       map.put(3,4)
+            stack.push(i);
+        }
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i]))
+            ans[i]=map.get(nums1[i]);
+            else ans[i]=-1;
+        }
+        return ans;
+        
+    }
+}
