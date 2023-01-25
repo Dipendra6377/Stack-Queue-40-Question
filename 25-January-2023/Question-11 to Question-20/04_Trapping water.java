f98584e57ad757bc36056d91a948727c989f93ab
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-01-25 22:53:25 +0530

    Update 04_Trapping water.java

--- 25-January-2023/Question-11 to Question-20/04_Trapping water.java
+++ 25-January-2023/Question-11 to Question-20/04_Trapping water.java
@@ -33,3 +33,40 @@ class Solution {
 
 
 // using Stack
+
+
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int l=0;
+        int r=n-1; int water=0;
+        int leftmaxheight=height[l];int rightmaxheight=height[r];
+
+        Stack<Integer> st=new Stack<>();
+        while(l<r){
+            if(height[l]>=leftmaxheight){
+                leftmaxheight=height[l];
+            }
+            else{
+                st.push(leftmaxheight-height[l]);
+            }
+
+            if(height[r]>=rightmaxheight){
+                rightmaxheight=height[r];
+            }
+            else{
+                st.push(rightmaxheight-height[r]);
+            }
+            if(rightmaxheight>leftmaxheight){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        while(!st.isEmpty()){
+            water+=st.pop();
+        }
+        return water;
+    }
+}
