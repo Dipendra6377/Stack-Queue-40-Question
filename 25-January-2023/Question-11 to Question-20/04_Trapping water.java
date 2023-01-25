018e12b0290a255799cbc06ec9a2cf710c3d5f19
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-01-25 20:51:18 +0530

    Create 04_Trapping water.java

---
+++ 25-January-2023/Question-11 to Question-20/04_Trapping water.java
@@ -0,0 +1,35 @@
+// time complexity O(N)
+
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int l=0;
+        int r=n-1; int res=0;
+        int leftmaxheight=0;int rightmaxheight=0;
+
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>=leftmaxheight){
+                    leftmaxheight=height[l];
+                }
+                else{
+                    res+=leftmaxheight-height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r]>=rightmaxheight){
+                    rightmaxheight=height[r];
+                }
+                else{
+                    res+=rightmaxheight-height[r];
+                }
+                r--;
+            }
+        }
+        return res;
+    }
+}
+
+
+// using Stack
