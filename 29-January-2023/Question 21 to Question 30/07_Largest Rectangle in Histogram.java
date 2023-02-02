c8bbe4dcbbb58599c2dc8ed4713161a93f31afbe
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-02-02 23:35:40 +0530

    Update and rename 06_Largest Rectangle in Histogram.java to 07_Largest Rectangle in Histogram.java

---
+++ 29-January-2023/Question 21 to Question 30/07_Largest Rectangle in Histogram.java
@@ -0,0 +1,56 @@
+// Naive
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea=0;
+        int n=heights.length;
+        for(int i=0;i<n;i++){
+            int min =Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                min =Math.min(min,heights[j]);
+                maxArea=Math.max(maxArea,min*(j-i+1));
+            }
+        }
+        return maxArea;
+    }
+}
+
+// better approach
+// time complexity O(N) space complexity O(3N)
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        Stack<Integer> st=new Stack<>();
+        int left[]=new int[n];
+        int right[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) left[i]=0;
+            else left[i]=st.peek()+1;
+
+            st.push(i);
+        }
+        while(!st.isEmpty()) st.pop();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) right[i]=n-1;
+            else right[i]=st.peek()-1;
+
+            st.push(i);
+        }
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            ans=Math.max(ans,(right[i]-left[i]+1)*heights[i]);
+        }
+        return ans;
+    }
+}
+
+// Optimised approach
